03c795282b3095dfe60ab05d6eebf6d24e51d672
Rui Ueyama <ruiu@google.com>
2013-08-01 23:41:06 -0700

    Add more tests.

--- test.sh
+++ test.sh
@@ -22,16 +22,24 @@ function run() {
 run integer 1 1
 run symbol a "'a"
 run quote a "(quote a)"
+run quote 63 "'63"
+run quote '(+ 1 2)' "'(+ 1 2)"
 run '+' 3 '(+ 1 2)'
 run list "(a b c)" "(list 'a 'b 'c)"
 
 run define 7 '(define x 7) x'
 run define 10 '(define x 7) (+ x 3)'
+run define 7 '(define + 7) +'
+run setq 11 '(define x 7) (setq x 11) x'
+run setq 17 '(setq + 17) +'
 
 run if a "(if 1 'a 'b)"
 run if a "(if 0 'a 'b)"
 run if a "(if 'x 'a 'b)"
 run if b "(if '() 'a 'b)"
+
+run = t '(= 3 3)'
+run = '()' '(= 3 2)'
 
 run lambda '<function>' '(lambda (x) x)'
 run lambda 9 '((lambda (x) (+ x x x)) 3)'
