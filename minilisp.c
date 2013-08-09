f3fb475a95aeee82055f691c529a482cafa7063d
Keita Obo <ktaobo@gmail.com>
2013-08-09 09:36:37 +0900

    To compile on Mac, use MAP_ANON

--- minilisp.c
+++ minilisp.c
@@ -11,6 +11,11 @@
 #include <string.h>
 #include <sys/mman.h>
 
+// For Mac.
+#ifndef MAP_ANONYMOUS
+# define MAP_ANONYMOUS MAP_ANON
+#endif
+
 //======================================================================
 // Lisp objects
 //======================================================================
