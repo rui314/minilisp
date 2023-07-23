b6aec1d76622cc66cfdcab2f763eae09b2f70ad5
nobutaka <n.takushima@gmail.com>
2023-07-24 00:23:39 +0900

    build with cc or gcc or zig

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-zig cc -s -Os -Wall -o minilisp minilisp.c
+if command -v cc > /dev/null 2>&1; then
+  CC=cc
+elif command -v gcc > /dev/null 2>&1; then
+  CC=gcc
+elif command -v zig > /dev/null 2>&1; then
+  CC='zig cc'
+fi
+
+$CC -s -Os -Wall -o minilisp minilisp.c
