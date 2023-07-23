46d66d9a54834ef2e019038e8f80efdd8ff0dbbe
nobutaka <n.takushima@gmail.com>
2023-07-24 01:38:23 +0900

    Use https://github.com/mstorsjo/llvm-mingw to build on windows.

--- build.sh
+++ build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-if command -v cc > /dev/null 2>&1; then
-  CC=cc
-elif command -v gcc > /dev/null 2>&1; then
-  CC=gcc
-elif command -v zig > /dev/null 2>&1; then
-  CC='zig cc'
-fi
-
-$CC -std=gnu99 -g -O2 -Wall -o minilisp minilisp.c
+cc -std=gnu99 -g -O2 -Wall -o minilisp minilisp.c
